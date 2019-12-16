c0a0fb6a266ad110a69d134e861b8ed70593969a
sreecherry2123 <54701051+sreecherry2123@users.noreply.github.com>
2019-12-16 15:29:54 +0530

    Update init.sql
    updated data

--- init_sql_scripts/init.sql
+++ init_sql_scripts/init.sql
@@ -4,6 +4,5 @@ create table country_and_capitals (
 );
 
 insert into country_and_capitals(country, capital) values ('India', 'Delhi');
-insert into country_and_capitals(country, capital) values ('China', 'Beijing');
-insert into country_and_capitals(country, capital) values ('Bhutan', 'Thimpu');
-insert into country_and_capitals(country, capital) values ('Russia', 'Moscow');
+insert into country_and_capitals(country, capital) values ('Malyisa', 'Koulalampur');
+insert into country_and_capitals(country, capital) values ('Abudabi', 'Dubai');
